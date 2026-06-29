42d95c5fd82c2bd5599731a2b68ea195220ae37c
ThiaggoSylva <thiaggol.sylva@gmail.com>
2026-06-29 19:32:18 -0300

    Adicionada as Views doModuloDespesa

--- EAgendaWeb.WebApp/obj/Debug/net10.0/EAgendaWeb.WebApp.AssemblyInfo.cs
+++ EAgendaWeb.WebApp/obj/Debug/net10.0/EAgendaWeb.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EAgendaWeb.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dda7cbd4c86dd3886815593eb0ba766c226a8f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d94810a74fa461caee1777eac96d730ba99f4f8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EAgendaWeb.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EAgendaWeb.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
